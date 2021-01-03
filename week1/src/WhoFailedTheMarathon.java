4fcbcbaf7882eff177a53713cc438c3d0994121b
Minjoon Choi <55074268+m-joon-ixix@users.noreply.github.com>
2021-01-03 23:58:56 +0900

    Update WhoFailedTheMarathon.java
    Reduced the complexity from n^2 to n*log(n)

--- week1/src/WhoFailedTheMarathon.java
+++ week1/src/WhoFailedTheMarathon.java
@@ -2,15 +2,16 @@
 Who Failed to Finish the Marathon? - Hash
 */
 
-// Correct, but Time-Out
+import java.util.Arrays;
 
 public class WhoFailedTheMarathon {
     public static void main(String[] args){
         String[] participant = {"mislav", "stanko", "mislav", "ana"};
-        String[] completion = {"mislav", "ana", "mislav"};
+        String[] completion = {"mislav", "ana", "stanko"};
         System.out.println(solution(participant, completion));
     }
 
+    /* Correct, but Time-Out -> Complexity is O(n^2)
     static String solution(String[] participant, String[] completion) {
         boolean[] succeeded = new boolean[participant.length];
 
@@ -27,5 +28,18 @@ public class WhoFailedTheMarathon {
             if(!succeeded[i]) return participant[i];
         }
         return "";
+    }
+    */
+
+    // Arrays.sort used for participant & completion, and then compare them! -> Complexity becomes O(n*logn)
+    static String solution(String[] participant, String[] completion) {
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        for(int i = 0; i < completion.length; i++){
+            if(!participant[i].equals(completion[i])) return participant[i];
+        }
+
+        return participant[participant.length - 1];
     }
 }
